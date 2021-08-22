8e99cb9ab12cea6b0a20b3655c7df2a410f861fb
Callum Mackenzie <Alexxander1611@gmail.com>
2021-08-22 12:42:38 -0600

    Fix installer and vulkan build.

--- tools/linux_install_build_tools.sh
+++ tools/linux_install_build_tools.sh
@@ -5,6 +5,9 @@ apt update
 # X11 tools
 apt install xorg-dev
 
+# Compiler
+apt install gcc g++ cmake
+
 # Vulkan install
 wget -qO - https://packages.lunarg.com/lunarg-signing-kek-pub.asc | apt-key add -
 wget -qO /etc/apt/sources.list.d/lunarg-vulkan-1.2.182-focal.list https://packages.lunarg.com/vulkan/1.2.182/lunarg-vulkan-1.2.182-focal.list
