5db7465ef17d8186b8439a8b72ecf131489d3f2a
Callum Mackenzie <61220096+CallumMackenzie@users.noreply.github.com>
2021-09-22 17:51:32 -0600

    Update linux-install-build-tools.sh

--- tools/linux-install-build-tools.sh
+++ tools/linux-install-build-tools.sh
@@ -2,24 +2,14 @@
 
 apt update
 
-# CMake
-apt install cmake
-
 # X11 tools
-apt install xorg-dev
-
-# Compiler
-apt install gcc g++ cmake
+apt install xorg-dev gcc g++ cmake gdb mesa-utils libglfw3-dev
 
 # Vulkan install
 wget -qO - https://packages.lunarg.com/lunarg-signing-kek-pub.asc | apt-key add -
 wget -qO /etc/apt/sources.list.d/lunarg-vulkan-1.2.182-focal.list https://packages.lunarg.com/vulkan/1.2.182/lunarg-vulkan-1.2.182-focal.list
 apt update
 apt install vulkan-sdk
-
-# OpenGL install
-apt install mesa-utils
-apt install libglfw3-dev
 
 # Freetype install
 apt install libfreetype-dev
